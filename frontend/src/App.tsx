cf9173a2f0f4113258f44a03db031141681fa19d
LauTortosa <lau.tortosa@gmail.com>
2025-02-18 17:29:05 +0100

    feat: add and configure daisyui dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
 
   return (
     <>
-        <h1>Safepi</h1>
+        <h1 class="text-3xl font-bold text-blue-500">Safepi</h1>
     </>
   )
 }
