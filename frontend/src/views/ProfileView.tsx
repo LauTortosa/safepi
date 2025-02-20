9a4a19e1d6a31928a763935fb1963b3edcb1b9ba
LauTortosa <lau.tortosa@gmail.com>
2025-02-20 07:34:35 +0100

    style: text and padding

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -8,23 +8,21 @@ const ProfileView: React.FC = () => {
   return (
     <div>
       <NavbarComponent />
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-        <div className="w-full max-w-4xl p-8 bg-white shadow-lg rounded-lg">
-          <h1 className="text-3xl font-bold text-center mb-6">Perfil</h1>
+      <div className="flex flex-col items-center bg-gray-100 pt-20">
+        <div className="w-full max-w-4xl p-8 bg-white shadow-lg rounded-lg pb-16 ">
+          <h1 className="text-3xl font-bold text-start mb-6">Información de perfil</h1>
           {user ? (
-            <div className="mt-4 text-center">
-              <p className="text-lg font-semibold">
-                Bienvenido, {user.name} {user.last_name}!
-              </p>
-              <p className="text-gray-600">Email: {user.email}</p>
-              <p className="text-gray-600">
+            <div className="mt-4 text-start">
+              <p className="text-gray-600 font-semibold mb-4">Nombre: {user.name} {user.last_name}</p>
+              <p className="text-gray-600 font-semibold mb-4">Email: {user.email}</p>
+              <p className="text-gray-600 font-semibold mb-4">
                 Fecha de nacimiento: {user.birthday}
               </p>
-              <p className="text-gray-600">
+              <p className="text-gray-600 font-semibold mb-4">
                 Fecha de inicio: {user.start_date}
               </p>
-              <p className="text-gray-600">Edad: {user.age}</p>
-              <p className="text-gray-600">Posición laboral: {user.position}</p>
+              <p className="text-gray-600 font-semibold mb-4">Edad: {user.age}</p>
+              <p className="text-gray-600 font-semibold">Posición laboral: {user.position}</p>
             </div>
           ) : (
             <p className="text-gray-500 text-center">Cargando información...</p>
