306b2b92aeb67005bb22778580c373e2329ea6ba
LauTortosa <lau.tortosa@gmail.com>
2025-02-20 07:43:25 +0100

    style: text center and justify

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -8,12 +8,13 @@ const ProfileView: React.FC = () => {
   return (
     <div>
       <NavbarComponent />
-      <div className="flex flex-col items-center bg-gray-100 pt-20">
+      <div className="min-h-screen flex flex-col items-center bg-gray-100 pt-20">
         <div className="w-full max-w-4xl p-8 bg-white shadow-lg rounded-lg pb-16 ">
-          <h1 className="text-3xl font-bold text-start mb-6">Información de perfil</h1>
+          <h1 className="text-3xl font-bold text-center mb-6">Información de perfil</h1>
           {user ? (
-            <div className="mt-4 text-start">
-              <p className="text-gray-600 font-semibold mb-4">Nombre: {user.name} {user.last_name}</p>
+            <div className="mt-4 text-center">
+                <div className="inline-block text-justify max-w-md">
+                <p className="text-gray-600 font-semibold mb-4">Nombre: {user.name} {user.last_name}</p>
               <p className="text-gray-600 font-semibold mb-4">Email: {user.email}</p>
               <p className="text-gray-600 font-semibold mb-4">
                 Fecha de nacimiento: {user.birthday}
@@ -23,6 +24,8 @@ const ProfileView: React.FC = () => {
               </p>
               <p className="text-gray-600 font-semibold mb-4">Edad: {user.age}</p>
               <p className="text-gray-600 font-semibold">Posición laboral: {user.position}</p>
+                </div>
+              
             </div>
           ) : (
             <p className="text-gray-500 text-center">Cargando información...</p>
