9845c3351ddbbe14f802239827ee8af6deec6c93
LauTortosa <lau.tortosa@gmail.com>
2025-03-01 07:50:04 +0100

    feat: add sort function to table headers add sorting icons for asc and desc use sortedItems to display sorted data

--- frontend/src/components/TableComponent.jsx
+++ frontend/src/components/TableComponent.jsx
@@ -1,16 +1,40 @@
+import React, { useState } from 'react';
+
 const TableComponent = ({ headers, rows, userRole, onDelete, onUpdate }) => {
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
+
+    const sortedItems = [...rows].sort((a, b) => {
+        if (sortConfig.key === null) return 0;
+        if (a[sortConfig.key] < b[sortConfig.key]) return sortConfig.direction === 'asc' ? -1 : 1;
+        if (a[sortConfig.key] > b[sortConfig.key]) return sortConfig.direction === 'asc' ? 1 : -1;
+        return 0;
+    });
+
+    const onSort = (key) => {
+        let direction = 'asc';
+
+        if (sortConfig.key === key && sortConfig.direction === 'asc') {
+            direction = 'desc';
+        }
+
+        setSortConfig({ key, direction });
+    }; 
+
     return (
         <div>
             <main className="flex-1 flex justify-center px-6">
                 <div className="bg-white shadow-lg rounded-lg p-8 mb-10 ml-80">
                     <div className="overflow-x-auto">
-                        {/* TODO orden ascendente/descendente */}
                         <table className="table-auto w-full border-collapse border border-gray-300">
                             <thead>
                                 <tr className="bg-blue-900 text-white">
                                     {headers.map((header, index) => (
-                                        <th key={index} className="p-2 border">
-                                            {header}
+                                        <th 
+                                            key={index} 
+                                            className="p-2 border"
+                                            onClick={() => onSort(index)}
+                                            >
+                                            {header} {sortConfig.key === index ? (sortConfig.direction === 'asc' ? '🔼' : '🔽') : ''}
                                         </th>
                                     ))}
                                     {userRole === "ADMIN" && (
@@ -19,8 +43,8 @@ const TableComponent = ({ headers, rows, userRole, onDelete, onUpdate }) => {
                                 </tr>
                             </thead>
                             <tbody>
-                                {rows.length > 0 ? (
-                                    rows.map((row, index) => (
+                                {sortedItems.length > 0 ? (
+                                    sortedItems.map((row, index) => (
                                         <tr key={index} className="hover:bg-gray-200">
                                             {row.map((cell, i) => (
                                                 <td key={i} className="p-2 border">{cell}</td>
