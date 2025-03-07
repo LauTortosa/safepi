fbdcc8d6bfb02c76b8f46a6b71821fc2a81eec96
LauTortosa <lau.tortosa@gmail.com>
2025-03-07 08:27:25 +0100

    feat: add validations

--- src/main/java/com/safepi/safepi/Entities/WorkEvent.java
+++ src/main/java/com/safepi/safepi/Entities/WorkEvent.java
@@ -1,10 +1,14 @@
 package com.safepi.safepi.Entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.safepi.safepi.Entities.Enums.Category;
 import com.safepi.safepi.Entities.Enums.Impact;
 import com.safepi.safepi.Entities.Enums.Location;
 import com.safepi.safepi.Entities.Enums.TypeWorkEvent;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
@@ -15,25 +19,37 @@ public class WorkEvent {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Category category;
 
+    @NotNull
+    @JsonFormat(pattern = "dd-MM-yyy")
     private LocalDate date;
 
+    @NotNull
+    @Size(min = 5, max = 350)
     private String description;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private TypeWorkEvent typeWorkEvent;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Location location;
 
+    @NotNull
+    @Size(min = 3, max = 25)
     private String affectedPerson;
 
+    @Size(min = 3, max = 25)
     private String witnesses;
 
+    @Size(min = 5, max = 350)
     private String firstAid;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Impact impact;
 }
