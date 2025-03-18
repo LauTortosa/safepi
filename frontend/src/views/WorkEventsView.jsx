e672f45e25fa730e75480d508e76d9109a6c5c5e
LauTortosa <lau.tortosa@gmail.com>
2025-03-18 07:43:28 +0100

    feat: add ContentBox component and useEffect with GET list method

--- frontend/src/views/WorkEventsView.jsx
+++ frontend/src/views/WorkEventsView.jsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import { useAuthUser } from "../hooks/useAuthUser";
+import api from "../api/axiosConfig";
+
+import ContentBoxComponent from "../components/ContentBoxComponent";
+
 const WorkEventsView = () => {
+    const [workEvents, setWorkEvents] = useState();
+    const { token, userRole } = useAuthUser();
+
+    useEffect(() => {
+        if (token) {
+            api.get("/workEvents", {headers: { Authorization: `Bearer ${token}` } 
+            })
+            .then((response) => {
+                setWorkEvents(response.data); 
+                console.log("response get", response.data);
+            })
+            .catch((error) => {
+                console.log("Error al obtener la lista de incidentes/accidentes", error);
+            })
+        } else { 
+            console.error("Token no encontrado");
+        }
+    }, []);
+
+
     return (
-        <div>
-            <h1>work events</h1>
-        </div>
+        <ContentBoxComponent
+            title={"INCIDENTES Y ACCIDENTES LABORALES"}
+            userRole={userRole}
+            sidebarOptions={[
+                { path: "", label: "Lista de Incidentes/Accidentes"},
+                { path: "", label: "Añadir Incidente/Accidente"},
+            ]}
+        >
+
+        </ContentBoxComponent>
     );
 };
 
