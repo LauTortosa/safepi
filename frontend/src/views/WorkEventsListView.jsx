2f31d89845a3d1107bc1eaf50f46f1f9ef6f76ce
LauTortosa <lau.tortosa@gmail.com>
2025-03-19 16:24:34 +0100

    feat: add delete function

--- frontend/src/views/WorkEventsListView.jsx
+++ frontend/src/views/WorkEventsListView.jsx
@@ -4,10 +4,21 @@ import api from "../api/axiosConfig";
 
 import ContentBoxComponent from "../components/ContentBoxComponent";
 import TableComponent from "../components/TableComponent";
+import ModalComponent from "../components/ModalComponent";
 
 const WorkEventsView = () => {
     const [workEvents, setWorkEvents] = useState([]);
     const { token, userRole } = useAuthUser();
+    const [isWorkEventDeleted, setIsWorkEventDeleted] = useState(false);
+
+    const rows = workEvents.map((workEvent, index) => [
+        index + 1,
+        "(" + workEvent.id + ") " + workEvent.name + " " + workEvent.last_name,
+        workEvent.date,
+        workEvent.category,
+        workEvent.typeWorkEvent,
+        workEvent.impact,
+    ]);
 
     useEffect(() => {
         if (token) {
@@ -24,16 +35,22 @@ const WorkEventsView = () => {
         }
     }, []);
 
-    const rows = workEvents.map((workEvent, index) => [
-        index + 1,
-        workEvent.id,
-        workEvent.date,
-        workEvent.category,
-        workEvent.typeWorkEvent,
-        workEvent.impact,
-        workEvent.name + " " + workEvent.last_name
-    ]);
-
+    const onDelete = (workEventId) => {
+        console.log("workeventid", workEventId);
+        if (token) {
+            api.delete(`/workEvents/${workEventId}`, { headers: { Authorization: `Bearer ${token}` }
+            })
+            .then(() => {
+                setWorkEvents((prevWorkEvents) => prevWorkEvents.filter((workEvents) => workEvents.id !== workEventId));
+                setIsWorkEventDeleted(true);
+            })
+            .catch((error) => {
+                console.error("Erro al eliminar el indicente/accidente", error);
+            })
+        } else {
+            console.error("Token no encontrado");
+        };
+    };
 
     return (
         <ContentBoxComponent
@@ -45,11 +62,17 @@ const WorkEventsView = () => {
             ]}
         >
             <TableComponent
-                headers={["#", "Id", "Fecha", "Categoria", "Tipo", "Impacto", "Nombre"]}
+                headers={["#", "Nombre", "Fecha", "Categoria", "Tipo", "Impacto", ]}
                 rows={rows}
                 userRole={userRole}
+                onDelete={onDelete}
             />
-
+            <ModalComponent 
+                isOpen={isWorkEventDeleted}
+                title={"Incidente/Accidente eliminado!"}
+                content={"El incidente/accidente ha sido eliminado correctamente."}
+                onClose={() => setIsWorkEventDeleted(false)}
+            />
         </ContentBoxComponent>
     );
 };
